aa38dc4c9557ca64b103f3fef94654892c00d747
Hack Reactor Students <kyle.warneck@gmail.com>
2013-12-09 15:46:20 -0800

    Second try intial commit

--- web/request-handler.js
+++ web/request-handler.js
@@ -1,6 +1,27 @@
 var path = require('path');
 module.exports.datadir = path.join(__dirname, "../data/sites.txt"); // tests will need to override this.
 
+var sendResponse = function(request, response, data){
+  response.writeHead(200, {
+    'content-type': 'text/html'
+  });
+  response.end(data);
+};
+
+var addURL = function(request, response){
+
+};
+
+var servePage = function(request, response){
+  sendResponse(request, response);
+};
+
+var router = {
+  'GET': servePage,
+  'POST': addURL
+};
+
 module.exports.handleRequest = function (req, res) {
+  router[req.method].call(this, req, res);
   console.log(exports.datadir);
 };
