ceb7958749c63beef1d114dc85da2e809323f7a9
Hack Reactor Students <kyle.warneck@gmail.com>
2013-12-09 21:05:42 -0800

    First attempt at workers

--- workers/htmlfetcher.js
+++ workers/htmlfetcher.js
@@ -1,2 +1,20 @@
-// eventually, you'll have some code here that uses the tested helpers 
+// eventually, you'll have some code here that uses the tested helpers
 // to actually download the urls you want to download.
+
+var fs = require('fs');
+var helpers = require('./lib/html-fetcher-helpers.js');
+
+module.exports.datadir = path.join(__dirname, "../data/sites.txt"); // tests will need to override this.
+
+module.exports.pagedir = path.join(__dirname, "../data/sites/"); // tests will need to override this.
+
+
+var urls = [];
+
+fs.readFile(exports.datadir,function(err,data){
+  if (err) {throw err;}
+  urls = data.split('\n');
+});
+
+helpers.downloadUrls(urls, exports.pagedir);
+
--- workers/lib/html-fetcher-helpers.js
+++ workers/lib/html-fetcher-helpers.js
@@ -1,7 +1,31 @@
-exports.readUrls = function(filePath, cb){
+var http = require('http');
 
+exports.readUrls = function(filePath, cb){
+  cb.call(this,fs.readFile(filePath));
 };
 
-exports.downloadUrls = function(urls){
-  // fixme
+exports.downloadUrls = function(urls , filePath){
+  for ( var i = 0 ; i < urls.length ; i++){
+    var options = {
+      hostname: urls[i],
+      port: 80,
+      method: 'GET'
+    };
+
+    var body;
+
+    http.request(options, function(res) {
+      res.setEncoding('utf8');
+      res.on('data', function (chunk) {
+        console.log('BODY: ' + chunk);
+        body += chunk;
+      });
+
+      res.on('end', function(body){
+        fs.writeFile(filePath+url[i], data, 'utf8', function(err){
+          if(err) throw err;
+        });
+      });
+    });
+  }
 };
