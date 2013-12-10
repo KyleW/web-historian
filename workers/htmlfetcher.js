af7b98d66762cda82cd67df3a44f5a0c7fc1d9ce
Hack Reactor Students <kyle.warneck@gmail.com>
2013-12-10 12:15:48 -0800

    It's ALIVEgit statusgit status

--- workers/htmlfetcher.js
+++ workers/htmlfetcher.js
@@ -3,10 +3,14 @@
 
 var fs = require('fs');
 var helpers = require('./lib/html-fetcher-helpers.js');
+// var helpers = require('code/kylew/2013-11-web-historian/workers/lib/html-fetcher-helpers.js'); //crontab version
 var path = require('path');
 
 module.exports.datadir = path.join(__dirname, "../data/sites.txt"); // tests will need to override this.
 module.exports.pagedir = path.join(__dirname, "../data/sites/"); // tests will need to override this.
+
+// module.exports.datadir = path.join(__dirname, "code/kylew/2013-11-web-historian/data/sites.txt"); // tests will need to override this.
+// module.exports.pagedir = path.join(__dirname, "code/kylew/2013-11-web-historian/data/sites/"); // tests will need to override this.
 
 console.log("htmlfetcher.js is running");
 helpers.downloadUrls();
